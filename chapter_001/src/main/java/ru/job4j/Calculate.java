af7cc74c089d7a1e6e0f327dbabe14b090e00b65
Tengu <seo@kts.by>
2017-07-27 19:40:07 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Vladimir Mailyanc (seo@kts.by)
+ * @version $Id.1$
+ * @since 0.1
+ */
 public class Calculate {
-    public static void main(String[] arg) {
+	 /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] arg) {	
+      //Вывод надписи на экран
         System.out.println("Hello world!");
       }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Vladimir Mailyanc (seo@kts.by)
+ * @version $Id.1$
+ * @since 0.1
+ */
+package ru.job4j;
