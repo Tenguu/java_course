d077cf9634bf12c2762a8d4e4c0fb99c231ccd64
Tengu <seo@kts.by>
2017-08-15 12:37:12 +0300

    Calculator

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,6 +3,6 @@ package ru.job4j.max;
 
 public class Max {
     public int max(int first, int second){
-        return Math.max(first, second);
+        return (first >= second) ? first : second;
     }
 }
