ba66408b776d683426effab3d9b5b1744f186965
Tengu <seo@kts.by>
2017-08-22 15:47:02 +0300

    Max

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,8 +1,15 @@
 package ru.job4j.max;
 
 
+
+
 public class Max {
     public int max(int first, int second){
-        return (first >= second) ? first : second;
+       int maximum = (first >= second) ? first : second;
+        return maximum;
+    }
+
+    public int max(int first, int second, int third){
+        return (Math.max(max(first, second),third));
     }
 }
